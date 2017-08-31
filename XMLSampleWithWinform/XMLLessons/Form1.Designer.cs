ae28f98ea266d8ff75b39f7e334ae6514ee9d541
Türhan Yıldırım <turhanyildirim@windowslive.com>
2017-08-31 19:13:34 +0300

    Xml project moved.

---
+++ XMLSampleWithWinform/XMLLessons/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace XMLLessons
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+			this.XMLDataGridView = new System.Windows.Forms.DataGridView();
+			this.ReadDataButton = new System.Windows.Forms.Button();
+			this.SearchXMLDataButton = new System.Windows.Forms.Button();
+			this.SearchXMLDataButton2 = new System.Windows.Forms.Button();
+			this.UpdateXMLDataButton = new System.Windows.Forms.Button();
+			this.SearchXMLDataButton3 = new System.Windows.Forms.Button();
+			this.DeleteXMLDataButton = new System.Windows.Forms.Button();
+			this.AddXMLDataButton = new System.Windows.Forms.Button();
+			this.SQLToXMLButton = new System.Windows.Forms.Button();
+			this.XMLToSQLButton = new System.Windows.Forms.Button();
+			this.GenerateXMLSchemaButton = new System.Windows.Forms.Button();
+			this.SQLToXMLWithSchemaButton = new System.Windows.Forms.Button();
+			this.XMLSchemaValidationButton = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.XMLDataGridView)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// XMLDataGridView
+			// 
+			dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+			dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+			dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+			dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+			dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+			this.XMLDataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+			this.XMLDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+			dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+			dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+			dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+			dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+			this.XMLDataGridView.DefaultCellStyle = dataGridViewCellStyle2;
+			this.XMLDataGridView.Location = new System.Drawing.Point(12, 12);
+			this.XMLDataGridView.Name = "XMLDataGridView";
+			dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+			dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+			dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+			dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+			dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+			this.XMLDataGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+			this.XMLDataGridView.Size = new System.Drawing.Size(597, 268);
+			this.XMLDataGridView.TabIndex = 0;
+			// 
+			// ReadDataButton
+			// 
+			this.ReadDataButton.Location = new System.Drawing.Point(12, 306);
+			this.ReadDataButton.Name = "ReadDataButton";
+			this.ReadDataButton.Size = new System.Drawing.Size(75, 23);
+			this.ReadDataButton.TabIndex = 1;
+			this.ReadDataButton.Text = "Veri Oku";
+			this.ReadDataButton.UseVisualStyleBackColor = true;
+			this.ReadDataButton.Click += new System.EventHandler(this.ReadDataButton_Click);
+			// 
+			// SearchXMLDataButton
+			// 
+			this.SearchXMLDataButton.Location = new System.Drawing.Point(93, 306);
+			this.SearchXMLDataButton.Name = "SearchXMLDataButton";
+			this.SearchXMLDataButton.Size = new System.Drawing.Size(75, 23);
+			this.SearchXMLDataButton.TabIndex = 2;
+			this.SearchXMLDataButton.Text = "Veri Bul";
+			this.SearchXMLDataButton.UseVisualStyleBackColor = true;
+			this.SearchXMLDataButton.Click += new System.EventHandler(this.SearchXMLDataButton_Click);
+			// 
+			// SearchXMLDataButton2
+			// 
+			this.SearchXMLDataButton2.Location = new System.Drawing.Point(93, 335);
+			this.SearchXMLDataButton2.Name = "SearchXMLDataButton2";
+			this.SearchXMLDataButton2.Size = new System.Drawing.Size(75, 23);
+			this.SearchXMLDataButton2.TabIndex = 3;
+			this.SearchXMLDataButton2.Text = "Veri Bul 2";
+			this.SearchXMLDataButton2.UseVisualStyleBackColor = true;
+			this.SearchXMLDataButton2.Click += new System.EventHandler(this.SearchXMLDataButton2_Click);
+			// 
+			// UpdateXMLDataButton
+			// 
+			this.UpdateXMLDataButton.Location = new System.Drawing.Point(174, 306);
+			this.UpdateXMLDataButton.Name = "UpdateXMLDataButton";
+			this.UpdateXMLDataButton.Size = new System.Drawing.Size(75, 23);
+			this.UpdateXMLDataButton.TabIndex = 4;
+			this.UpdateXMLDataButton.Text = "Veri Değiştir";
+			this.UpdateXMLDataButton.UseVisualStyleBackColor = true;
+			this.UpdateXMLDataButton.Click += new System.EventHandler(this.UpdateXMLDataButton_Click);
+			// 
+			// SearchXMLDataButton3
+			// 
+			this.SearchXMLDataButton3.Location = new System.Drawing.Point(93, 364);
+			this.SearchXMLDataButton3.Name = "SearchXMLDataButton3";
+			this.SearchXMLDataButton3.Size = new System.Drawing.Size(75, 23);
+			this.SearchXMLDataButton3.TabIndex = 5;
+			this.SearchXMLDataButton3.Text = "Veri Bul 3";
+			this.SearchXMLDataButton3.UseVisualStyleBackColor = true;
+			this.SearchXMLDataButton3.Click += new System.EventHandler(this.SearchXMLDataButton3_Click);
+			// 
+			// DeleteXMLDataButton
+			// 
+			this.DeleteXMLDataButton.Location = new System.Drawing.Point(255, 306);
+			this.DeleteXMLDataButton.Name = "DeleteXMLDataButton";
+			this.DeleteXMLDataButton.Size = new System.Drawing.Size(75, 23);
+			this.DeleteXMLDataButton.TabIndex = 6;
+			this.DeleteXMLDataButton.Text = "Veri Sil";
+			this.DeleteXMLDataButton.UseVisualStyleBackColor = true;
+			this.DeleteXMLDataButton.Click += new System.EventHandler(this.DeleteXMLDataButton_Click);
+			// 
+			// AddXMLDataButton
+			// 
+			this.AddXMLDataButton.Location = new System.Drawing.Point(336, 306);
+			this.AddXMLDataButton.Name = "AddXMLDataButton";
+			this.AddXMLDataButton.Size = new System.Drawing.Size(75, 23);
+			this.AddXMLDataButton.TabIndex = 7;
+			this.AddXMLDataButton.Text = "Veri Ekle";
+			this.AddXMLDataButton.UseVisualStyleBackColor = true;
+			this.AddXMLDataButton.Click += new System.EventHandler(this.AddXMLDataButton_Click);
+			// 
+			// SQLToXMLButton
+			// 
+			this.SQLToXMLButton.Location = new System.Drawing.Point(417, 306);
+			this.SQLToXMLButton.Name = "SQLToXMLButton";
+			this.SQLToXMLButton.Size = new System.Drawing.Size(75, 23);
+			this.SQLToXMLButton.TabIndex = 8;
+			this.SQLToXMLButton.Text = "SQL to XML";
+			this.SQLToXMLButton.UseVisualStyleBackColor = true;
+			this.SQLToXMLButton.Click += new System.EventHandler(this.SQLToXMLButton_Click);
+			// 
+			// XMLToSQLButton
+			// 
+			this.XMLToSQLButton.Location = new System.Drawing.Point(417, 335);
+			this.XMLToSQLButton.Name = "XMLToSQLButton";
+			this.XMLToSQLButton.Size = new System.Drawing.Size(75, 23);
+			this.XMLToSQLButton.TabIndex = 9;
+			this.XMLToSQLButton.Text = "XML to SQL";
+			this.XMLToSQLButton.UseVisualStyleBackColor = true;
+			this.XMLToSQLButton.Click += new System.EventHandler(this.XMLToSQLButton_Click);
+			// 
+			// GenerateXMLSchemaButton
+			// 
+			this.GenerateXMLSchemaButton.Location = new System.Drawing.Point(498, 306);
+			this.GenerateXMLSchemaButton.Name = "GenerateXMLSchemaButton";
+			this.GenerateXMLSchemaButton.Size = new System.Drawing.Size(111, 23);
+			this.GenerateXMLSchemaButton.TabIndex = 10;
+			this.GenerateXMLSchemaButton.Text = "Schema Oluştur";
+			this.GenerateXMLSchemaButton.UseVisualStyleBackColor = true;
+			this.GenerateXMLSchemaButton.Click += new System.EventHandler(this.GenerateXMLSchemaButton_Click);
+			// 
+			// SQLToXMLWithSchemaButton
+			// 
+			this.SQLToXMLWithSchemaButton.Location = new System.Drawing.Point(498, 335);
+			this.SQLToXMLWithSchemaButton.Name = "SQLToXMLWithSchemaButton";
+			this.SQLToXMLWithSchemaButton.Size = new System.Drawing.Size(111, 52);
+			this.SQLToXMLWithSchemaButton.TabIndex = 11;
+			this.SQLToXMLWithSchemaButton.Text = "SQL to XML with Schema";
+			this.SQLToXMLWithSchemaButton.UseVisualStyleBackColor = true;
+			this.SQLToXMLWithSchemaButton.Click += new System.EventHandler(this.SQLToXMLWithSchemaButton_Click);
+			// 
+			// XMLSchemaValidationButton
+			// 
+			this.XMLSchemaValidationButton.Location = new System.Drawing.Point(498, 393);
+			this.XMLSchemaValidationButton.Name = "XMLSchemaValidationButton";
+			this.XMLSchemaValidationButton.Size = new System.Drawing.Size(111, 52);
+			this.XMLSchemaValidationButton.TabIndex = 12;
+			this.XMLSchemaValidationButton.Text = "XML Schema Validation";
+			this.XMLSchemaValidationButton.UseVisualStyleBackColor = true;
+			this.XMLSchemaValidationButton.Click += new System.EventHandler(this.XMLSchemaValidationButton_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(629, 460);
+			this.Controls.Add(this.XMLSchemaValidationButton);
+			this.Controls.Add(this.SQLToXMLWithSchemaButton);
+			this.Controls.Add(this.GenerateXMLSchemaButton);
+			this.Controls.Add(this.XMLToSQLButton);
+			this.Controls.Add(this.SQLToXMLButton);
+			this.Controls.Add(this.AddXMLDataButton);
+			this.Controls.Add(this.DeleteXMLDataButton);
+			this.Controls.Add(this.SearchXMLDataButton3);
+			this.Controls.Add(this.UpdateXMLDataButton);
+			this.Controls.Add(this.SearchXMLDataButton2);
+			this.Controls.Add(this.SearchXMLDataButton);
+			this.Controls.Add(this.ReadDataButton);
+			this.Controls.Add(this.XMLDataGridView);
+			this.Name = "Form1";
+			this.Text = "XML Örnekleri";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.XMLDataGridView)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView XMLDataGridView;
+		private System.Windows.Forms.Button ReadDataButton;
+		private System.Windows.Forms.Button SearchXMLDataButton;
+		private System.Windows.Forms.Button SearchXMLDataButton2;
+		private System.Windows.Forms.Button UpdateXMLDataButton;
+		private System.Windows.Forms.Button SearchXMLDataButton3;
+		private System.Windows.Forms.Button DeleteXMLDataButton;
+		private System.Windows.Forms.Button AddXMLDataButton;
+		private System.Windows.Forms.Button SQLToXMLButton;
+		private System.Windows.Forms.Button XMLToSQLButton;
+		private System.Windows.Forms.Button GenerateXMLSchemaButton;
+		private System.Windows.Forms.Button SQLToXMLWithSchemaButton;
+		private System.Windows.Forms.Button XMLSchemaValidationButton;
+	}
+}
+
---
+++ XMLSampleWithWinform/XMLLessons/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Xml;
+//XPath name space ekliyoruz
+using System.Xml.XPath;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace XMLLessons
+{
+	public partial class Form1 : Form
+	{
+		String XMLFilePath = Application.StartupPath + "//EmployeeList.xml";
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void ReadDataButton_Click(object sender, EventArgs e)
+		{
+			//XML veri tipinde data verir
+			DataSet DS = new DataSet();
+			DS.ReadXml(XMLFilePath);
+
+			XMLDataGridView.DataSource = DS.Tables[0];
+
+		}
+
+		private void SearchXMLDataButton_Click(object sender, EventArgs e)
+		{
+			XmlDocument XMLFile = new XmlDocument();
+			XMLFile.Load(XMLFilePath);
+
+			XmlNode SelectedXMLNode = XMLFile.ChildNodes[1];
+
+			foreach (XmlNode item in SelectedXMLNode)//Calısan
+			{
+				//if (item.ChildNodes[0].InnerText == "Zübeyir")//Adı
+				if (item.Attributes["TCNo"].Value == "12345678902")
+				{
+					MessageBox.Show("Eleman bulundu: \n" + item.ChildNodes[0].InnerText + " " + item.ChildNodes[1].InnerText + "\n" + item.ChildNodes[2].InnerText);
+					break;
+				}
+			}
+
+		}
+
+		private void SearchXMLDataButton2_Click(object sender, EventArgs e)
+		{
+			//Xpath - Daha hızlı arama yapılmasını sağlamaktadır
+			//@ işreti attribute anlamına gelmektedir
+
+			XmlDocument XMLFile = new XmlDocument();
+			XMLFile.Load(XMLFilePath);
+
+			//Calisanlar içinde olan Calisan nesnelerinin içinede TCNo = 12345678903 olan node bana bul
+			XmlNode SelectedXMLNode = XMLFile.SelectSingleNode("Calisanlar/Calisan[@TCNo=12345678903]");
+
+			MessageBox.Show("Eleman bulundu: \n" + SelectedXMLNode.ChildNodes[0].InnerText + " " + SelectedXMLNode.ChildNodes[1].InnerText + "\n" + SelectedXMLNode.ChildNodes[2].InnerText);
+
+		}
+		 
+		private void SearchXMLDataButton3_Click(object sender, EventArgs e)
+		{
+			//Elementlerde innertext, Attributelerde ise valu kullanılır datayı almak için
+
+			XPathDocument XMLFile = new XPathDocument(XMLFilePath);
+			XPathNavigator XPathNav = XMLFile.CreateNavigator();
+			XPathNodeIterator SelectedNode = XPathNav.Select("Calisanlar/Calisan/Adi");
+
+			String NameString = String.Empty;
+			while (SelectedNode.MoveNext())
+			{
+				NameString +=  SelectedNode.Current.InnerXml + "\n";
+			}
+
+			MessageBox.Show(NameString);
+		}
+
+		private void UpdateXMLDataButton_Click(object sender, EventArgs e)
+		{
+			XmlDocument XMLFile = new XmlDocument();
+			XMLFile.Load(XMLFilePath);
+
+			//Calisanlar içinde olan Calisan nesnelerinin içinede TCNo = 12345678903 olan node bana bul
+			XmlNode SelectedXMLNode = XMLFile.SelectSingleNode("Calisanlar/Calisan[@TCNo=12345678903]");
+
+			if (SelectedXMLNode != null)
+			{
+				SelectedXMLNode.ChildNodes[0].InnerText = "Fatma";
+				XMLFile.Save(XMLFilePath);
+
+				MessageBox.Show("Veri güncellendi.");
+			}
+			else
+			{
+				MessageBox.Show("Kişi bulunamadı.");
+			}
+		}
+
+		private void DeleteXMLDataButton_Click(object sender, EventArgs e)
+		{
+			XmlDocument XMLFile = new XmlDocument();
+			XMLFile.Load(XMLFilePath);
+
+			//Calisanlar içinde olan Calisan nesnelerinin içinede TCNo = 12345678903 olan node bana bul
+			XmlNode SelectedXMLNode = XMLFile.SelectSingleNode("Calisanlar/Calisan[@TCNo=12345678903]");
+
+			if (SelectedXMLNode != null)
+			{
+				XMLFile.DocumentElement.RemoveChild(SelectedXMLNode);
+				XMLFile.Save(XMLFilePath);
+
+				MessageBox.Show("Veri Silindi");
+			}
+			else
+			{
+				MessageBox.Show("Silinecek veri yok!");
+			}
+		}
+
+		private void AddXMLDataButton_Click(object sender, EventArgs e)
+		{
+			XmlDocument XMLFile = new XmlDocument();
+			XMLFile.Load(XMLFilePath);
+
+			//En üst node oluşturulması
+			XmlElement MyElement = XMLFile.CreateElement("Calisan");
+			//Calisan nodunun TCNo attribte oluşturulması
+			XmlAttribute MyElementAttribute = XMLFile.CreateAttribute("TCNo");
+			//TCNo attribute değer atanması
+			MyElementAttribute.Value = "12345678904";
+			//Attribute Calisan içine eklenmesi
+			MyElement.Attributes.Append(MyElementAttribute);
+
+
+			//Adi nodunun oluştururlması
+			XmlNode XMlAdiNode = XMLFile.CreateNode(XmlNodeType.Element, "Adi","");
+			//Adi node içine değer atanması
+			XMlAdiNode.InnerText = "Buse";
+			MyElement.AppendChild(XMlAdiNode);
+
+			//Soyadi nodunun oluştururlması
+			XmlNode XMlSoyadiNode = XMLFile.CreateNode(XmlNodeType.Element, "Soyadi", "");
+			//Soyadi node içine değer atanması
+			XMlSoyadiNode.InnerText = "HAVALI";
+			MyElement.AppendChild(XMlSoyadiNode);
+
+			//DogumYili nodunun oluştururlması
+			XmlNode XMlDogumYiliNode = XMLFile.CreateNode(XmlNodeType.Element, "DogumYili", "");
+			//Soyadi node içine değer atanması
+			XMlDogumYiliNode.InnerText = "HAVALI";
+			MyElement.AppendChild(XMlDogumYiliNode);
+
+			XMLFile.DocumentElement.AppendChild(MyElement);
+
+			XMLFile.Save(XMLFilePath);
+
+			MessageBox.Show("Veri eklendi.");
+		}
+
+		private void SQLToXMLButton_Click(object sender, EventArgs e)
+		{
+            //Kullanmak için aşağıdaki linkte yer alan AdventureWorks2008R2_SR1.exe dosya ile AdventureWorksLT database kurunuz
+            //http://msftdbprodsamples.codeplex.com/releases/view/55926
+
+            SqlConnection Conn = new SqlConnection("server=.; Database=AdventureWorksLT; trusted_connection=true");
+			SqlDataAdapter SDAdapter = new SqlDataAdapter("Select * From [SalesLT].[Product]", Conn);
+			DataTable DTable = new DataTable("Product");
+			SDAdapter.Fill(DTable);
+
+			DataSet DS = new DataSet("Products");
+			DS.Tables.Add(DTable);
+
+			DS.WriteXml(Application.StartupPath + "\\SqlToXML.xml");
+
+			MessageBox.Show("Sql datası XML olarak kaydedildi"); 
+		}
+
+		private void XMLToSQLButton_Click(object sender, EventArgs e)
+		{
+			FolderBrowserDialog FDialog = new FolderBrowserDialog();
+			DialogResult DResult = FDialog.ShowDialog();
+
+			if (DResult == System.Windows.Forms.DialogResult.OK)
+			{
+				String XMLFile = FDialog.SelectedPath + "\\SqlToXml.xml";
+
+				if (File.Exists(XMLFile))
+				{
+					SqlConnection Conn = new SqlConnection("server=.; Database=AdventureWorksLT; trusted_connection=true");
+					SqlDataAdapter SDAdapter = new SqlDataAdapter("Select * From [SalesLT].[Product]", Conn);
+
+					SqlCommandBuilder SComman = new SqlCommandBuilder(SDAdapter);
+
+					DataSet DS = new DataSet();
+					DS.ReadXml(XMLFile);
+
+					SDAdapter.Update(DS.Tables[0]);
+
+					MessageBox.Show("XML data Veri tabanın eklendi!");
+				}
+				else
+				{
+					MessageBox.Show("Dosya bulunamadı \n SqlToXML.xml");
+				}
+			}
+			else
+			{
+				MessageBox.Show("Dosya seçilmedi!");
+			}
+		}
+
+		private void GenerateXMLSchemaButton_Click(object sender, EventArgs e)
+		{
+			SqlConnection Conn = new SqlConnection("server=.; Database=AdventureWorksLT; trusted_connection=true");
+			SqlDataAdapter SDAdapter = new SqlDataAdapter("Select * From [SalesLT].[Product]", Conn);
+
+			DataSet DS = new DataSet("XUrunler");
+			DataTable DTable = new DataTable("XUrun");
+			SDAdapter.Fill(DTable);
+
+			DS.Tables.Add(DTable);
+
+			String XMLSchemaFilePath = Application.StartupPath + "\\MySchema.xsd";
+			DS.WriteXmlSchema(XMLSchemaFilePath);
+
+			MessageBox.Show("Schema oluşturuldu.");
+		}
+
+		private void SQLToXMLWithSchemaButton_Click(object sender, EventArgs e)
+		{
+			//Schema dosyası adresi
+			String SchemaFile = Application.StartupPath + "\\MySchema.xsd";
+
+			//AS ile aslında schema içindeki alanlar ile eşleştirilmeli 
+			SqlConnection Conn = new SqlConnection("server=.; Database=AdventureWorksLT; trusted_connection=true");
+			SqlDataAdapter SDAdapter = new SqlDataAdapter("Select Name AS Name , ProductNumber AS ProductNumber,Color AS Color From [SalesLT].[Product]", Conn);
+			DataTable DTable = new DataTable("XUrun");
+			DTable.ReadXmlSchema(SchemaFile);
+			SDAdapter.Fill(DTable);
+
+			DataSet DS = new DataSet("XUrunler");
+			DS.Tables.Add(DTable);
+
+			DS.WriteXml(Application.StartupPath + "\\SqlToXML.xml");
+
+			MessageBox.Show("Sql datası XML olarak kaydedildi"); 
+		}
+
+		private void XMLSchemaValidationButton_Click(object sender, EventArgs e)
+		{
+			//Schema dosyası adresi
+			String SchemaFile = Application.StartupPath + "\\MySchema.xsd";
+
+			//XML reader ayarlarının yapılması
+			XmlReaderSettings XMLReadSet = new XmlReaderSettings();
+			XMLReadSet.Schemas.Add("", SchemaFile);
+			XMLReadSet.ValidationType = ValidationType.Schema;
+			XMLReadSet.ValidationEventHandler += new System.Xml.Schema.ValidationEventHandler(XMLReadSet_ValidationEventHandler);
+
+
+			XmlReader XReader = XmlReader.Create(XMLFilePath);
+
+			while (XReader.Read())
+			{
+				
+			}
+
+			if (ErrorMessage == String.Empty)
+			{
+				//Schema ile XML uyumludur
+				MessageBox.Show("OK");
+			}
+			else
+			{
+				MessageBox.Show(ErrorMessage);
+			}
+		}
+
+		String ErrorMessage = String.Empty;
+		void XMLReadSet_ValidationEventHandler(object sender, System.Xml.Schema.ValidationEventArgs e)
+		{
+			if (e.Severity == System.Xml.Schema.XmlSeverityType.Error)
+			{
+				ErrorMessage += "Hata Mesajı: " + e.Message + "\r\n";
+			}
+			else  
+			{
+				ErrorMessage += "Uyarı Mesajı: " + e.Message + "\r\n";
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
