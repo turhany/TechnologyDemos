d3f84e773aae0f96599502df650f9e249cfb232b
Türhan Yıldırım <turhanyildirim@windowslive.com>
2017-08-31 19:04:01 +0300

    SyntaxHighlight project moved.

---
+++ SyntaxHighlightSample/SyntaxHigh/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SyntaxHigh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(5, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(570, 315);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 339);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ SyntaxHighlightSample/SyntaxHigh/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace SyntaxHigh
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            const string tokens = "(test|keyword|highlight)";
+
+            Regex rex = new Regex(tokens);
+
+            MatchCollection mc = rex.Matches(richTextBox1.Text);
+            int startCursorPosition = richTextBox1.SelectionStart;
+            foreach (Match m in mc)
+            {
+                var startIndex = m.Index;
+                var stopIndex = m.Length;
+
+                richTextBox1.Select(startIndex, stopIndex);
+                richTextBox1.SelectionColor = Color.Blue;
+                richTextBox1.SelectionStart = startCursorPosition;
+                richTextBox1.SelectionColor = Color.Black;
+            }
+        }
+    }
+}
